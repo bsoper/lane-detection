aab929c20ad9d512930e07eef14dae9ddc96a932
JaeHP <JaeHP@users.noreply.github.com>
2017-04-11 11:06:25 -0400

    Update main.py
    Made the code compatible with new warper.py
    -Added remove src.npy from data upon completion
    -Removed unnecessary warper.set_transfrom line

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from scipy import misc
 import numpy as np
+import os
 
 from polydrawer import Polydrawer
 from polyfitter import Polyfitter
@@ -28,9 +29,9 @@ def main(video_name='other_video'):
 
     white_output = '{}_done_2.mp4'.format(video_name)
     clip1 = VideoFileClip('{}.mp4'.format(video_name))#.subclip(0, 5)
-    warper.set_transforms(clip1.size)
     white_clip = clip1.fl_image(process_image)  # NOTE: this function expects color images!!
     white_clip.write_videofile(white_output, audio=False)
+    os.remove('data/src.npy')
 
 
 def process_image(base):
